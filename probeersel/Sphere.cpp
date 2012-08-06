d95418fb0b7c311cce198e19da3daa0feba9f980
Laurens Bossen <laurensbossen@hotmail.com>
2012-08-06 21:55:21 +0200

    When the sphere is small than a certain value, replace it by a dot

--- probeersel/Sphere.cpp
+++ probeersel/Sphere.cpp
@@ -17,20 +17,31 @@ private:
 public:
     void draw()
     {
-        float angle(0.0);
+        if (_radius < 10)
+        {
+            glPointSize(16.0f);
+            glColor3f(0.0f, 1.0f, 1.0f);
+            glBegin(GL_POINTS);
+                glVertex2f(-50, -70);
+            glEnd();
+        }
+        else
+        {
+            float angle(0.0);
 
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glLineWidth(2.0f);
-        glLineStipple(2, 0x3F07);
-        glEnable(GL_LINE_STIPPLE);
-        glBegin(GL_LINE_LOOP);
-            for (int i=0; i < segments; ++i)
-            {
-                angle = 2*PI*i/segments;
-                glVertex2f(_radius*cos(angle), _radius*sin(angle));
-            }
-        glEnd();
-        glDisable(GL_LINE_STIPPLE);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glLineWidth(2.0f);
+            glLineStipple(2, 0x3F07);
+            glEnable(GL_LINE_STIPPLE);
+            glBegin(GL_LINE_LOOP);
+                for (int i=0; i < segments; ++i)
+                {
+                    angle = 2*PI*i/segments;
+                    glVertex2f(_radius*cos(angle), _radius*sin(angle));
+                }
+            glEnd();
+            glDisable(GL_LINE_STIPPLE);
+        }
     }
 
     // constructors
