40b5c90905c5d6d2e7e4a820311508f8689d19aa
Laurens Bossen <laurensbossen@hotmail.com>
2012-08-25 14:34:41 +0200

    changed graphics such that the point of the 'kite' is the front of the ship

--- probeersel/ShipGraphicsComponent.cpp
+++ probeersel/ShipGraphicsComponent.cpp
@@ -16,12 +16,12 @@ void ShipGraphicsComponent::draw(ShipPhysicsComponent const& pysicsComp)
 //    glRectf(-25.0, -25.0, 25.0, 25.0);  // draw a rectangle
     glBegin(GL_POLYGON);
         glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f(-25, -25, 0.0);
-        glVertex3f(25, -25, 0.0);
+        glVertex3f(0.0, 35, 0.0);
+        glVertex3f(-25, 0.0, 0.0);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(35, 35, 0.0);
+        glVertex3f(0.0, -25, 0.0);
         glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f(-25, 25, 0.0);
+        glVertex3f(25, 0.0, 0.0);
     glEnd();
 
     glPopMatrix();
